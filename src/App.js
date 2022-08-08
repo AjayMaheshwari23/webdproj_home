fce12cb4baa03093909a149e0866522a21b54fe3
Ajay <lci2021023@iiitl.ac.in>
2022-08-08 15:32:55 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Login from './components/login/login';
+import Homepage from './components/homepage/homepage';
+import Register from './components/register/register';
 
 function App() {
   return (
     <div className="App">
-          Learn React
+    <Login/>
+      <Register/>
+      <Homepage/>
       </div>
   );
 }
